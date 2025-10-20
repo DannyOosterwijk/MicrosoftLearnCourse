d7eb67022c3ab8b7efc759ad3fb011890cc96c55
Danny Oosterwijk <danny@oosterwijk.net>
2025-10-20 15:19:07 +0200

    [ADD] guided project petting zoo finished

---
+++ 23-PettingZoo/Program.cs
@@ -0,0 +1,70 @@
+﻿// guided project visit petting zoo
+
+using System;
+
+string[] pettingZoo =
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+InputSchool("School A", 6);
+InputSchool("School B", 3);
+InputSchool("School C", 2);
+
+//enter the school name and amount of groups. will assign animals to all the different groups and print them in order
+void InputSchool(string schoolName, int AmountOfGroups = 1)
+{
+    RandomizeAnimals();
+    string[,] group = AssignGroup(AmountOfGroups);
+    Console.WriteLine(schoolName);
+    PrintGroup(group);
+}
+
+
+//randomize animals by randomly swapping array positions
+void RandomizeAnimals()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++)
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[r];
+        pettingZoo[r] = pettingZoo[i];
+        pettingZoo[i] = temp;
+    }
+}
+
+//split the animals equally among the groups and return the split groups
+string[,] AssignGroup(int groups = 6)
+{
+    string[,] result = new string[groups, pettingZoo.Length / groups];
+    int start = 0;
+
+    for (int i = 0; i < groups; i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
+}
+
+
+void PrintGroup(string[,] groups) 
+{
+    for (int i = 0; i < groups.GetLength(0); i++) 
+    {
+        Console.Write($"Group {i + 1}: ");
+        for (int j = 0; j < groups.GetLength(1); j++) 
+        {
+            Console.Write($"{groups[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
