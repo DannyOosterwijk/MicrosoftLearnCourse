fcd63beb3ed9beedd30763039a10735b9c408fac
Danny Oosterwijk <danny@oosterwijk.net>
2025-10-20 14:33:01 +0200

    [ADD] finished return values

---
+++ 22-ReturnValues/Program.cs
@@ -0,0 +1,61 @@
+﻿// methods that return values, Challenge
+using System.Diagnostics;
+
+Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay()) 
+{
+    PlayGame();
+}
+
+void PlayGame()
+{
+    var play = true;
+
+    while (play)
+    {
+        var target = random.Next(1, 6);
+        var roll = random.Next(1, 7);
+
+        Console.WriteLine($"Roll a number greater than {target} to win!");
+        Console.WriteLine($"You rolled a {roll}");
+        Console.WriteLine(WinOrLose(target, roll));
+        Console.WriteLine("\nPlay again? (Y/N)");
+
+        play = ShouldPlay();
+    }
+}
+
+string WinOrLose(int target, int roll)
+{
+    if(roll > target)
+    {
+        return "You Win!";
+    }
+    else
+    {
+        return "You Lose!";
+    }
+}
+
+bool ShouldPlay()
+{
+    while (true)
+    {
+        string input = Console.ReadLine();
+        if(input != null)
+        {
+            input = input.ToLower().Trim();
+            if (input == "y")
+            {
+                return true;
+            }
+            else if (input == "n")
+            {
+                return false;
+            }
+        }
+    }
+    
+}
