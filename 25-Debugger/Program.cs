485408d9978230d49c1569d6c28c8267b7a74340
Danny Oosterwijk <danny@oosterwijk.net>
2025-10-22 16:04:52 +0200

    [ADD] starting on debugging file

---
+++ 25-Debugger/Program.cs
@@ -0,0 +1,36 @@
+﻿//implement the visual studio code debbuging tools
+
+/* 
+This code uses a names array and corresponding methods to display
+greeting messages
+*/
+
+string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
+
+foreach (string name in names)
+{
+    string messageText = "";
+
+
+    if (name == "Sophia")
+        messageText = SophiaMessage();
+    else if (name == "Andrew")
+        messageText = AndrewMessage();
+    else if (name == "AllGreetings")
+        messageText = SophiaMessage() + "\n\r" + AndrewMessage();
+
+    Console.WriteLine(messageText + "\n\r");
+}
+
+bool pauseCode = true;
+while (pauseCode == true);
+
+static string SophiaMessage()
+{
+    return "Hello, my name is Sophia.";
+}
+
+static string AndrewMessage()
+{
+    return "Hi, my name is Andrew. Good to meet you.";
+}
